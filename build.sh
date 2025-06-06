4ba12710553eb85a45810075e553e4c5a9e1a4eb
Mohit Verma <sinhamohit6226@gmail.com>
2025-06-06 15:37:13 +0530

    Added build.sh file

---
+++ build.sh
@@ -0,0 +1,11 @@
+# Exit on error
+set -o errexit
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Apply database migrations
+python manage.py migrate
+
+# Collect Static Files
+python manage.py collectstatic
